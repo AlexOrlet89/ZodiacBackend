44c744ec2824385535c3f88f30b96a8c482dfc17
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-06 17:52:07 -0700

    first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,19 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiac } = require('../data/zodiac');
 
 describe('zodiac backend routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiac should return a list of zodiac signs', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiac.map((z) => {
+      return { id: z.id, name: z.name };
+    });
+    expect(res.body).toEqual(expected);
   });
 
   afterAll(() => {
